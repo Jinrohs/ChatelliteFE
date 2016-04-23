0d62096bea2871453f665f2b41c77c6b5081a182
Hirokazu Yokoyama <hiyokoya@yahoo-corp.jp>
2016-04-23 14:54:31 +0900

    defaultの描画1

--- server/controllers/CzmlController.js
+++ server/controllers/CzmlController.js
@@ -5,10 +5,8 @@
 var fs = require('fs');
 
 var renderDefault = function(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
     fs.readFile(`${__dirname}/../public/default.czml`, 'UTF-8', function(err, data) {
-        res.write(JSON.stringify(data));
-        res.end();
+        res.json(JSON.parse(data));
     });
 };
 
@@ -16,8 +14,8 @@ module.exports = {
     handler: function(req, res) {
         var command = req.params.command;
         var option = req.params.option;
-        
-        if(command == 'default') {
+        console.log("cmd:" + command);
+        if(command === 'default') {
             renderDefault(req, res);
         }
     }
--- source/main.js
+++ source/main.js
@@ -2,7 +2,8 @@
 
 $(document).ready(function() {
     var viewer = new Cesium.Viewer('cesiumContainer');
-    var data = Cesium.CzmlDataSource.load('/api/czml/defualt');
-    
+    var data = Cesium.CzmlDataSource.load('/api/czml/default');
+    //var data = Cesium.CzmlDataSource.load('/default.czml');    
+    console.log(data);
     viewer.dataSources.add(data);
 });
