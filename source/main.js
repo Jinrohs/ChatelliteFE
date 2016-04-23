86e353bda984752cda1347630291b609c69f0ec3
masanari hamada <mahamada@yahoo-corp.jp>
2016-04-23 17:12:29 +0900

    ポップアップの追加

--- source/main.js
+++ source/main.js
@@ -16,9 +16,7 @@ $(document).ready(function() {
         baseLayerPicker : false
     });
 
-    var layers = viewer.imageryLayers;
-
-    var blackMarble = layers.addImageryProvider(Cesium.createTileMapServiceImageryProvider({
+    var blackMarble = viewer.imageryLayers.addImageryProvider(Cesium.createTileMapServiceImageryProvider({
         url: 'https://cesiumjs.org/blackmarble',
         credit: 'Black Marble imagery courtesy NASA Earth Observatory',
         flipXY: true
@@ -26,6 +24,27 @@ $(document).ready(function() {
     blackMarble.alpha = 0.9;
     blackMarble.brightness = 2.0;
 
+    // データ追加
     var data = Cesium.CzmlDataSource.load('/api/czml/default');
     viewer.dataSources.add(data);
+
+    // 衛星のクリックイベント
+    var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+
+    handler.setInputAction(function(event) {
+        console.log(event);
+
+        var pickedObject = viewer.scene.pick(event.position);
+
+        if (Cesium.defined(pickedObject)) {
+            console.log(pickedObject);
+            $('#popup').removeClass('hide');
+        }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+
+    // ポップアップ背景のクリックイベント
+    $('#popup').on('click', function (event) {
+        $(event.target).addClass('hide');
+    });
 });
