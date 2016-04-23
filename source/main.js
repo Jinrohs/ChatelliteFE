4cb97499fd89ccb807fc0e73f3476944a774669e
masanari hamada <mahamada@yahoo-corp.jp>
2016-04-23 15:14:46 +0900

    CSSとmainjsの更新

--- source/main.js
+++ source/main.js
@@ -1,9 +1,35 @@
 'use strict';
 
 $(document).ready(function() {
+<<<<<<< Updated upstream
     var viewer = new Cesium.Viewer('cesiumContainer');
     var data = Cesium.CzmlDataSource.load('/api/czml/default');
     //var data = Cesium.CzmlDataSource.load('/default.czml');    
     console.log(data);
+=======
+    var blackmarbleImageryProvider = Cesium.createTileMapServiceImageryProvider({
+        url : '//cesiumjs.org/tilesets/imagery/blackmarble',
+        minimumLevel : 0,
+        maximumLevel : 8
+    });
+
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        timeline: false,
+        navigationHelpButton: false,
+        infoBox: false,
+        homeButton: false,
+        geocoder: false,
+        animation: false,
+        navigationInstructionsInitiallyVisible: false,
+    });
+
+    var layers = viewer.scene.imageryLayers;
+
+    // console.log(layers)
+    // var blackMarble = layers.add(blackmarbleImageryProvider);
+    // console.log(Cesium);
+
+    var data = Cesium.CzmlDataSource.load('/api/czml/defualt');
+>>>>>>> Stashed changes
     viewer.dataSources.add(data);
 });
