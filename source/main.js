bc7daaf143d9e808a389d2ebb5bc57675f70b3cc
Hirokazu Yokoyama <hiyokoya@yahoo-corp.jp>
2016-04-23 12:01:43 +0900

    czmlのルーティングを追加

--- source/main.js
+++ source/main.js
@@ -1,19 +1,7 @@
 'use strict';
 
-var viewer = new Cesium.Viewer('cesiumContainer', {
-    fullscreenElement: 'previewContent'
-});
-
-var move = function() {
-    var camera = viewer.camera;
-    var positionCartographic = camera.positionCartographic;
-    var height = positionCartographic.height;
-    var lat = positionCartographic.latitude + Cesium.Math.toRadians(2.0);
-    var lon = positionCartographic.longitude + Cesium.Math.toRadians(20.0);
-    camera.flyTo({
-        destination: Cesium.Cartesian3.fromRadians(lon, lat, height),
-        duration: 1.0
-    });
-};
-
-showSource('cesiumSource', move);
+$(document).ready(function() {
+    var viewer = new Cesium.Viewer('cesiumContainer');
+    //var data = Cesium.CzmlDataSource.load('/api/czml/defualt');
+    //viewer.dataSources.add(data);
+})
