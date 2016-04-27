0703920fc41a7fbe97f24e05f76c86c5128c329f
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2016-04-27 22:16:50 +0900

    言語処理APIへのアクセスをフロントエンドサーバを経由するように変更 #17

---
+++ server/controllers/speech-controller.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var configure = require('../configure');
+var superagent = require('superagent');
+
+var loadSpeech = function(id, timestamp, callback) {
+    var params = {
+        id: id,
+        timestamp: timestamp
+    };
+    
+    superagent
+        .get(configure.speechApi)
+        .query(params)
+        .end(function(err, res) {            
+            callback(res.body);
+        });
+};
+
+module.exports = {
+    handler: function(req, res) {
+        var id = req.params.id;
+        var timestamp = req.params.timestamp;
+        console.log(id);
+        loadSpeech(id, timestamp, function(data) {
+            res.json(data);
+        });
+    }
+};
