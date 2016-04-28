ca67f0a2daf2aeba95904be893a42002570f1899
Hirokazu Yokoyama <hiyokoya@yahoo-corp.jp>
2016-04-28 19:58:45 +0900

    設定変更

--- server/configure.js
+++ server/configure.js
@@ -1,6 +1,6 @@
 module.exports = {
     "orbitApi": "210.140.86.209",
     "orbitApiPort": 5000,
-    "speechApi": "localhost:30000",    
+    "speechApi": "chatellite.spaceapps.jp:30000",    
     "multiplier": 20,
 };
